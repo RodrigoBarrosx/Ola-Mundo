25e5148537e5bb52a509d98dce99eabe9357988d
RodrigoBarrosx <131710150+RodrigoBarrosx@users.noreply.github.com>
2023-04-25 00:37:32 -0300

    Exercicios livro

---
+++ Livro Java script/001.js
@@ -0,0 +1,8 @@
+var book = {
+    topic: 'JavaScript',
+    fat: true
+};
+book.topic
+book.['fat']
+book.author = "Flanagan"
+book.contents = {}
---
+++ Livro Java script/002.js
@@ -0,0 +1,3 @@
+var prime = [2, 3, 5, 7]
+primes[0]
+primes.lenght
